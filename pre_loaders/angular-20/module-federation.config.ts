0bb283bbb25b5dc158cb5bb873ef2f5bea7d5391
Martin Kuczynski <129275100+markuczy@users.noreply.github.com>
2026-05-07 15:41:16 +0200

    fix: angular 20 preloader sharing

--- pre_loaders/angular-20/module-federation.config.ts
+++ pre_loaders/angular-20/module-federation.config.ts
@@ -1,5 +1,26 @@
-import { ModuleFederationConfig } from "@nx/module-federation";
+import {
+  ModuleFederationConfig,
+  SharedLibraryConfig,
+} from "@nx/module-federation";
 import { getOneCXSharedRecommendations } from "@onecx/accelerator";
+
+import * as pkg from "package.json";
+
+const additionalShared = Object.keys(pkg.dependencies)
+  .map((d) => {
+    return {
+      libraryName: d,
+      sharedConfig: getOneCXSharedRecommendations(d, {}),
+    };
+  })
+  .filter(
+    (
+      config,
+    ): config is { libraryName: string; sharedConfig: SharedLibraryConfig } =>
+      !!config.sharedConfig,
+  );
+
+console.log("aS", additionalShared);
 
 const config: ModuleFederationConfig = {
   name: "onecx-angular-20-loader",
@@ -11,6 +32,7 @@ const config: ModuleFederationConfig = {
     // Add custom shared configurations to the config object if needed
     return config;
   },
+  additionalShared: additionalShared,
 };
 
 export default config;
